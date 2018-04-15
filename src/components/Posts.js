a92248e8f071108a834bdec532c4f8342c1c087e
Joao Paulo <joaoded@hotmail.com>
2018-04-15 16:14:37 -0300

    Listing items...

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 
 class Posts extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
   componentWillMount() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ posts: data }));
   }
 
   render() {
+    const post_items = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
     return (
       <div>
         <h2>Posts</h2>
+        { post_items }
       </div>
     )
   }
