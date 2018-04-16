0574a8d4ef828deb25388e9d6ef5f7d5827e8c91
Joao Paulo <joaoded@hotmail.com>
2018-04-16 10:52:57 -0300

    Created PostForm component

---
+++ src/components/PostForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add Posts</h2>
+      </div>
+    )
+  }
+}
+
+export default PostForm;
