19e28c661a3608b88f769d7bdc68bad544d60fb4
Joao Paulo <joaoded@hotmail.com>
2018-04-20 17:33:14 -0300

    Few additions

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -8,10 +8,14 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
+      /*state = {
+        items: action.payload,
+        item: state.item
+      }*/
       return {
         ...state,
         items: action.payload
-      };
+      }
     default:
       return state;
   }
